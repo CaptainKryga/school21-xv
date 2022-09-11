ccc7e38b5c1a8d6886f6ebfaeed634703205a020
CaptainKryga <krygadenis@gmail.com>
2022-09-11 10:59:51 +0200

    init animation's/sequenc'es

---
+++ Assets/Project/Scripts/Model/Animation/ModelAnimation.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Project.Scripts.Model.Animation
+{
+	public class ModelAnimation : MonoBehaviour
+	{
+		
+	}
+}
---
+++ Assets/Project/Scripts/View/WindowAnimation.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Project.Scripts.View
+{
+	public class WindowAnimation : MonoBehaviour
+	{
+		public void OnClick_OpenRedactor()
+		{
+			
+		}
+
+		public void OnClick_StartSequence()
+		{
+			
+		}
+
+		public void OnClick_StopSequence()
+		{
+			
+		}
+	}
+}
